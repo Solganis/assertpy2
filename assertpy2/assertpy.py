3f37f46083026848b35dfdd5fdb8595174e137c4
Solganis <solganis.dev@gmail.com>
2026-09-23 17:36:38 +0300

    chore: release 2.27.0

--- assertpy2/assertpy.py
+++ assertpy2/assertpy.py
@@ -85,7 +85,7 @@ from .snapshot import SnapshotMixin
 from .string import StringMixin
 from .warning import WarningMixin
 
-__version__ = "2.26.0"
+__version__ = "2.27.0"
 
 # the tracked value type of the generic AssertionBuilder fallback (_U appears only in narrowing stubs)
 _T = TypeVar("_T")
